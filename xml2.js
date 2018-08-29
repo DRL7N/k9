2a8b2302b30eaea0bdda0a87c8562ee7875f188b
DRL7N <40870933+DRL7N@users.noreply.github.com>
2018-08-29 05:06:57 +0300

    Update xml2.js

--- xml2.js
+++ xml2.js
@@ -1,59 +1,32 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity("You",{type: 'WATCHING'})
 
-  console.log('GG')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
 });
 
-client.on("guildMemberRemove", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-تعال وما بقالي يوم 
-  في غيابك يصبرني .. !! 
-https://discord.gg/HxPDpbA
-الدعووة خاصة لك يا  [ ${member}  ]
-**`) 
-}).catch(console.error)
+ client.on('message', message => {
+  if (message.content === '-1-') {   
+      if (message.author.id !== '447179855898083338')
+ return message.react('⚠')
+    const channel = message.member.voiceChannel;
 
-})
- 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-   return channel.send(` 
-**
-تعال وما بقالي يوم 
-  في غيابك يصبرني .. !! 
-https://discord.gg/HxPDpbA
-الدعووة خاصة لك يا  [ ${member}  ]
-**`) 
-}).catch(console.error)
+    channel.join()
+    .then(connection => console.log('Connected!'))
+    .catch(console.error);
+  }
+});
 
-})
+ client.on('message', message => {
+  if (message.content === '-2-') {   
+      if (message.author.id !== '447179855898083338')
+ return message.react('🤦🏻‍♂️')
+    const channel = message.member.voiceChannel;
 
+    channel.leave()
+  }
+});
 
 
 
